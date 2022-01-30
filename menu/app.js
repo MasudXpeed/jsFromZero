7e6a8244bf3ed888c8765b58dc63c02648509d8c
Masud Rana <87362771+MasudXpeed@users.noreply.github.com>
2022-01-31 01:38:32 +0600

    30 project started

---
+++ menu/app.js
@@ -0,0 +1,15 @@
+'use strict'
+const menuHandelar = document.querySelector('.menu_handaler');
+const sidePanel = document.querySelector('.side-panel');
+
+menuHandelar.addEventListener('click', function () {
+    sidePanel.classList.toggle('active');
+})
+document.addEventListener('mouseup', function (e) {
+    if (e.target !== sidePanel && e.target !== menuHandelar) {
+        sidePanel.classList.remove('active');
+    }
+})
+
+
+
