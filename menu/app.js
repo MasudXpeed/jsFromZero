7426980cf6927e02134a973a419e4cee31a6d3ea
Masud Rana <87362771+MasudXpeed@users.noreply.github.com>
2022-03-08 14:50:26 +0600

    Side panel added

--- menu/app.js
+++ menu/app.js
@@ -2,11 +2,14 @@
 const menuHandelar = document.querySelector('.menu_handaler');
 const sidePanel = document.querySelector('.side-panel');
 
-menuHandelar.addEventListener('click', function () {
+menuHandelar.addEventListener('click', function (e) {
     sidePanel.classList.toggle('active');
 })
-document.addEventListener('mouseup', function (e) {
-    if (e.target !== sidePanel || e.target !== menuHandelar) {
+
+document.addEventListener('click', (e) => {
+    let el = e.target.closest('.side-panel');
+    let el2 = e.target.closest('.menu_handaler');
+    if (el2 == null && el == null) {
         sidePanel.classList.remove('active');
     }
 })
