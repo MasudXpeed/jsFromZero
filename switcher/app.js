ae86fcda4d43cf49732d9a30102b5332f1db6d80
Masud Rana <87362771+MasudXpeed@users.noreply.github.com>
2022-03-10 15:55:38 +0600

    Updated

--- switcher/app.js
+++ switcher/app.js
@@ -4,8 +4,10 @@ button.addEventListener('click', function (e) {
     button.classList.toggle('clicked');
     if (button.classList.contains('clicked')) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
     }
 });
 
